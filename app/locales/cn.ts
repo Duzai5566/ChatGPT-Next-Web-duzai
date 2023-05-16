637338dc9298f4876f736e3729b1b0d0d46427ee
Duzai5566 <129352630+Duzai5566@users.noreply.github.com>
2023-05-16 10:22:38 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，点击立即领取访问密码：https://s.r.sn.cn/nr6VuE",
+      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，点击立即领取访问密码：https://d57.life/KxZB4",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
